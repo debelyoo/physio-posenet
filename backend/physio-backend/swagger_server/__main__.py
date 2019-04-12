bf52d1e70265e518b859166f14111d3c1be5b9c3
Adriano Pagano <adriano@sqooba.io>
2019-04-13 00:57:58 +0200

    CORS

--- backend/physio-backend/swagger_server/__main__.py
+++ backend/physio-backend/swagger_server/__main__.py
@@ -2,6 +2,7 @@
 
 import connexion
 
+from flask_cors import CORS
 from swagger_server import encoder
 
 
@@ -9,6 +10,7 @@ def main():
     app = connexion.App(__name__, specification_dir='./swagger/')
     app.app.json_encoder = encoder.JSONEncoder
     app.add_api('swagger.yaml', arguments={'title': 'Physio PoseNet'})
+    CORS(app.app)
     app.run(port=8080)
 
 
