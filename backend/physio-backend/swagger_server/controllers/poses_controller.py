ba4098da839559196739f1c7bf880f3374cca637
Jean Rossier <jean@sqooba.io>
2019-04-13 01:55:19 +0200

    Integrate Pose table with SQLAlchemy

--- backend/physio-backend/swagger_server/controllers/poses_controller.py
+++ backend/physio-backend/swagger_server/controllers/poses_controller.py
@@ -3,7 +3,14 @@ import six
 
 from swagger_server.models.tag import Tag  # noqa: E501
 from swagger_server import util
+from flask import Response
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+import uuid
+from ..models.pose import Pose
 
+engine = create_engine('sqlite:////Users/jean.rossier/tools/arkathon/physio-posenet/db/physio.sqlite')
+Pose.__table__.create(engine)
 
 def add_pose(file):  # noqa: E501
     """Add a new pose to the library
@@ -15,7 +22,18 @@ def add_pose(file):  # noqa: E501
 
     :rtype: None
     """
-    return 'do some magic!'
+
+    session = sessionmaker(bind=engine)()
+    try:
+        pose_uuid = str(uuid.uuid4())
+        pose = Pose(uuid=pose_uuid, keypoints='x,x,x')
+        session.add(pose)
+        session.commit()
+    except Exception as e:
+        print(e)
+        session.rollback()
+
+    return Response("{'message':'Pose uploaded !'}", status=201, mimetype='application/json')
 
 
 def get_pose_by_id(poseId):  # noqa: E501
