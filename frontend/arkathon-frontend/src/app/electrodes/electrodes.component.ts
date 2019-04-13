d3f41fd76939ce57ee0128c55458888583b986df
TAAMEFL2 <florence.meier@swisscom.com>
2019-04-13 22:45:46 +0200

    Add IWILLNEVERMERGETHATSHIT for picture

--- frontend/arkathon-frontend/src/app/electrodes/electrodes.component.ts
+++ frontend/arkathon-frontend/src/app/electrodes/electrodes.component.ts
@@ -1,4 +1,8 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild, ElementRef } from '@angular/core';
+import { Subscription } from 'rxjs';
+import { ActivatedRoute } from '@angular/router';
+import { BackendServiceService } from '../services/backend-service.service';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-electrodes',
@@ -7,9 +11,66 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ElectrodesComponent implements OnInit {
 
-  constructor () { }
+  private URL = 'https://physio.test.sqooba.io/api/PICTURE_ENDPOINT/';
+  private readonly routeSubscription: Subscription;
+  private backendSubscription: Subscription;
+
+  @ViewChild('videoElement2') videoElement2: any;
+  private video2: any;
+  public videoOn = false;
+  public interval;
+
+  @ViewChild('canvas2')
+  public canvas2: ElementRef;
+
+  public picture;
+
+  constructor (private route: ActivatedRoute,
+    private backendService: BackendServiceService,
+    private http: HttpClient) {
+      /* this. backendSubscription = backendService.getPicture()
+      .subscribe(
+        (pose) => {
+          this.picture = pose;
+      }); */
+   }
 
   ngOnInit () {
+    this.video2 = this.videoElement2.nativeElement;
   }
 
+  public record() {
+    this.initCamera({ video: true, audio: false });
+    this.interval = setInterval(() => {
+      let ctx = this.canvas2.nativeElement.getContext('2d');
+      ctx.drawImage(this.video2, 0, 0, this.canvas2.nativeElement.width, this.canvas2.nativeElement.height);
+      let img = new Image();
+      img.src = this.canvas2.nativeElement.toDataURL("image/png");
+      /* this.http.post(this.URL + 1337 + "/check", img).subscribe(() => console.log(1)); */
+    }, 1000)
+  }
+
+  private initCamera (config: any) {
+    const browser = <any>navigator;
+
+    browser.getUserMedia = (browser.getUserMedia ||
+      browser.webkitGetUserMedia ||
+      browser.mozGetUserMedia ||
+      browser.msGetUserMedia);
+
+    browser.mediaDevices.getUserMedia(config)
+    .then((stream) => {
+      this.video2.srcObject = stream;
+      this.video2.play();
+    });
+  }
+
+  ngOnDestroy () {
+    if (this.routeSubscription) {
+      this.routeSubscription.unsubscribe();
+    }
+    if (this.backendSubscription) {
+      this.backendSubscription.unsubscribe();
+    }
+  }
 }
--- frontend/arkathon-frontend/src/app/exercise/exercise.component.ts
+++ frontend/arkathon-frontend/src/app/exercise/exercise.component.ts
@@ -1,8 +1,8 @@
-import { Component, OnInit, OnDestroy, ViewChild, ElementRef } from '@angular/core';
+import { Component, OnInit, OnDestroy, ViewChild, ElementRef, AfterViewInit } from '@angular/core';
 import { BackendServiceService } from '../services/backend-service.service';
 import { ActivatedRoute } from '@angular/router';
 import { Subscription } from 'rxjs';
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 
 @Component({
   selector: 'app-exercise',
@@ -10,7 +10,7 @@ import { HttpClient } from '@angular/common/http';
   styleUrls: ['./exercise.component.css']
 })
 export class ExerciseComponent implements OnInit, OnDestroy {
-  private URL = 'https://physio.test.sqooba.io/api/poses/';
+  private URL = 'https://physio.test.sqooba.io/api';
   private readonly routeSubscription: Subscription;
   private backendSubscription: Subscription;
 
@@ -19,28 +19,22 @@ export class ExerciseComponent implements OnInit, OnDestroy {
   public videoOn = false;
   public interval;
 
+  public IWILLNEVERMERGETHATSHIT;
+
   @ViewChild("canvas")
   public canvas: ElementRef;
-
-  public picture = '';
 
   constructor (private route: ActivatedRoute,
     private backendService: BackendServiceService,
     private http: HttpClient) {
-      this.routeSubscription = route.params.subscribe((param) => {
-        if (param.id) {
-          this. backendSubscription = backendService.getOnePose(0)
-          .subscribe(
-            (pose) => {
-              this.picture = pose;
-            }
-          );
-        }
-      });
    }
 
   ngOnInit () {
     this.video = this.videoElement.nativeElement;
+    this.backendSubscription = this.backendService.getPoses()
+      .subscribe((pose) => {
+      this.IWILLNEVERMERGETHATSHIT = this.URL + pose[0].thumbnail
+    });
   }
 
   public takePicture() {
@@ -50,7 +44,7 @@ export class ExerciseComponent implements OnInit, OnDestroy {
       ctx.drawImage(this.video, 0, 0, this.canvas.nativeElement.width, this.canvas.nativeElement.height);
       let img = new Image();
       img.src = this.canvas.nativeElement.toDataURL("image/png");
-      this.http.post(this.URL + 1337 + "/check", img).subscribe(res => console.log(res));
+      this.http.post(this.URL + 1337 + "/check", img).subscribe(() => console.log(1));
     }, 1000)
   }
 
