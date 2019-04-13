da3c7a6650d7f955f6f33abfd5e8765a67a56740
Jean Rossier <jean@sqooba.io>
2019-04-13 03:08:27 +0200

    remove absolute path

--- backend/physio-backend/swagger_server/pose/pose_handler.py
+++ backend/physio-backend/swagger_server/pose/pose_handler.py
@@ -4,11 +4,14 @@ from sqlalchemy.orm import sessionmaker
 import uuid
 from ..models.pose import Pose
 
-engine = create_engine('sqlite:////Users/jean.rossier/tools/arkathon/physio-posenet/db/physio.sqlite')
+db_folder = os.path.realpath('..') + "/db"
+os.makedirs(db_folder, exist_ok=True)
+engine = create_engine('sqlite:///'+ db_folder +'/physio.sqlite')
 
 # drop table at every boot of the application
-Pose.__table__.drop(engine)
-Pose.__table__.create(engine)
+table_name = "poses"
+if not engine.dialect.has_table(engine.connect(), table_name):
+    Pose.__table__.create(engine)
 
 def save_pose(file):
     session = sessionmaker(bind=engine)()
@@ -24,6 +27,9 @@ def save_pose(file):
             os.makedirs(os.path.dirname(fullPath), exist_ok=True)
             file.save(fullPath)
 
+        # TODO - extract keypoints
+
+        # persist in DB
         pose = Pose(uuid=pose_uuid, keypoints='x,x,x')
         session.add(pose)
         session.commit()
