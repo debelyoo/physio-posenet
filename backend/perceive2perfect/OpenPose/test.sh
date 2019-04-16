95c57ed4a373cc530618ef9f1d5ab1cbca3907df
Jean Rossier <jean@sqooba.io>
2019-04-16 07:36:40 +0200

    remove reference folders (bis)

--- backend/perceive2perfect/OpenPose/test.sh
+++
@@ -1,2 +0,0 @@
-sshpass -p Avalls2018bcn ssh  Avalls2018bcn@azure.rabbyte.club -t 'cd perceive2perfect/OpenPose/ && python OpenPoseImage.py > Output-Keypoints.txt'
-sshpass -p Avalls2018bcn scp  Avalls2018bcn@azure.rabbyte.club:~/perceive2perfect/OpenPose/Output-Keypoints.txt . 
